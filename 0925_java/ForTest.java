144eb3f6c629ee1689f26ea186858f608fc3dd27
hyyyyyo <152476824+hyyyyyo@users.noreply.github.com>
2025-09-25 15:00:17 +0900

    Add files via upload

---
+++ 0925_java/ForTest.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+		int[] scores = {91, 75, 84, 83, 87};
+		
+		int sum = 0;
+		for(int i=0; i<scores.length; i++) {
+			sum += scores[i];
+			// TODO Auto-generated method stub
+		}
+		
+		System.out.println("Sum : " + sum);
+
+	}
+
+}
