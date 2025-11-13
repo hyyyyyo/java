6dcec835f78a8745ea4e67597f6aa3dcccc04fa4
hyyyyyo <152476824+hyyyyyo@users.noreply.github.com>
2025-11-13 15:29:24 +0900

    Add files via upload

---
+++ 1113/exam03/Main.java
@@ -0,0 +1,28 @@
+package exam03;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Button btn = new Button();
+
+	        Button.OnClickListener CallListener =
+	            new Button.OnClickListener() {
+	                public void OnClick() {
+	                    System.out.println("Make a call!");
+	                }
+	            };
+
+	        btn.SetOnClickListener(CallListener);
+	        btn.Touch();
+
+	        btn.SetOnClickListener(
+	            new Button.OnClickListener() {
+	                public void OnClick() {
+	                    System.out.println("Send a message!!");
+	                }
+	            }
+	        );
+
+	        btn.Touch();
+	    }
+	}
