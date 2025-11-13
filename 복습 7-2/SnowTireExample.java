210ddfaf683d6c82641891b7aaeea27beef57015
hyyyyyo <152476824+hyyyyyo@users.noreply.github.com>
2025-11-13 15:54:38 +0900

    Add files via upload

---
+++ 복습 7-2/SnowTireExample.java
@@ -0,0 +1,16 @@
+package exam02;
+
+public class SnowTireExample {
+
+	public static void main(String[] args) {
+		SnowTire snowTire = new SnowTire();
+		Tire tire = snowTire;
+		
+		snowTire.run();
+		tire.run();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
